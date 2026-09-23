2ddac244c191c6ac8e10e842b46c3d8700d42363
vistart <i@vistart.me>
2026-09-23 22:34:50 +0800

    test(ddl): classify CommentSupport for Firebird conformance

--- tests/rhosocial/activerecord_firebird_test/feature/backend/test_firebird_protocol_conformance.py
+++ tests/rhosocial/activerecord_firebird_test/feature/backend/test_firebird_protocol_conformance.py
@@ -68,6 +68,7 @@ FIREBIRD_PROTOCOLS = [
     dialect_protocols.AutoIncrementSupport,
     dialect_protocols.CTESupport,
     dialect_protocols.ColumnAttributeSupport,
+    dialect_protocols.CommentSupport,
     dialect_protocols.CollationSupport,
     dialect_protocols.ConstraintSupport,
     dialect_protocols.DDLTypeSupport,
